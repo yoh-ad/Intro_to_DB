308ae9efb90893f181c68c8cbe556659832bd137
yoh-ad <yohad2004@gmail.com>
2025-06-29 21:11:05 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,3 @@
--- List all tables in the current database (alx_book_store)
-
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = DATABASE();
+-- List all tables in the alx_book_store database
+USE alx_book_store;
+SHOW TABLES;
