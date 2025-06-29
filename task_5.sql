af81550b3231106bc99971a53ce581b5a4b056ee
yoh-ad <yohad2004@gmail.com>
2025-06-29 21:15:55 +0300

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- task_5.sql - Insert single customer record
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (
+    1,
+    'Cole Baidoo',
+    'cbaidoo@sandtech.com',
+    '123 Happiness Ave.'
+);
