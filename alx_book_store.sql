c15a94591bba6539f77744656b106ec99852f41c
yoh-ad <yohad2004@gmail.com>
2025-06-29 10:04:29 +0300

    alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,11 +1,14 @@
+-- Create the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
+-- Authors table
 CREATE TABLE authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
+-- Books table
 CREATE TABLE books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
@@ -15,6 +18,7 @@ CREATE TABLE books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
+-- Customers table
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
@@ -22,6 +26,7 @@ CREATE TABLE customers (
     address TEXT
 );
 
+-- Orders table
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
@@ -29,6 +34,7 @@ CREATE TABLE orders (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
+-- Order_Details table
 CREATE TABLE order_details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
@@ -37,4 +43,3 @@ CREATE TABLE order_details (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
-
