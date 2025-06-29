1a89dda492783b6fb75a779086e6f542888b45c7
yoh-ad <yohad2004@gmail.com>
2025-06-29 21:13:11 +0300

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Full description of Books table using INFORMATION_SCHEMA
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
